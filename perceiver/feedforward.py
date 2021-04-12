89fbf18aabf7c3808c85699978826a946f54af76
Rishit Dagli <rishit.dagli@gmail.com>
2021-04-12 20:46:56 +0530

    Format with black

--- perceiver/feedforward.py
+++ perceiver/feedforward.py
@@ -1,14 +1,18 @@
 import tensorflow as tf
 from .geglu import GEGLU
 
+
 class FeedForward(tf.keras.layers.Layer):
-    def __init__(self, dim, mult=4, dropout=0.):
+    def __init__(self, dim, mult=4, dropout=0.0):
         super(FeedForward, self).__init__()
-        self.net = tf.keras.Sequential([tf.keras.layers.Dense(dim * mult * 2, input_dim=dim),
-                                        GEGLU(),
-                                        tf.keras.layers.Dropout(dropout),
-                                        tf.keras.layers.Dense(dim, input_dim=dim * mult)
-                                        ])
+        self.net = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(dim * mult * 2, input_dim=dim),
+                GEGLU(),
+                tf.keras.layers.Dropout(dropout),
+                tf.keras.layers.Dense(dim, input_dim=dim * mult),
+            ]
+        )
 
     def call(self, inputs):
         return self.net(inputs)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-exec(open('perceiver/version.py').read())
+exec(open("perceiver/version.py").read())
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -10,10 +10,8 @@ setup(
     version="0.1.0",
     description="Implement of Perceiver, General Perception with Iterative Attention in TensorFlow",
     packages=["perceiver"],
-
     long_description=long_description,
     long_description_content_type="text/markdown",
-
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
@@ -21,7 +19,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        'License :: OSI Approved :: MIT License',
+        "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
@@ -30,30 +28,23 @@ setup(
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Mathematics"
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
-
     keywords=[
         "perceiver",
         "artificial intelligence",
         "deep learning",
         "transformer",
-        "attention mechanism"
+        "attention mechanism",
     ],
     url="https://github.com/Rishit-dagli/Perceiver",
     author="Rishit Dagli",
     author_email="rishit.dagli@gmail.com",
-
     install_requires=[
         "tensorflow~=2.4.0",
         "einops>=0.3",
     ],
-
     extras_require={
-        "dev": [
-            "check-manifest",
-            "twine",
-            "numpy"
-        ],
+        "dev": ["check-manifest", "twine", "numpy"],
     },
 )
